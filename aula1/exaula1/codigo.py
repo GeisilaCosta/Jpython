5ff8199afc8b758181f70c9a99d02b93582e82f5
GeisilaCosta <geisilascosta@gmail.com>
2025-01-16 08:51:43 -0300

    adicionando projetos

---
+++ aula1/exaula1/codigo.py
@@ -0,0 +1,13 @@
+# passo 1 : Abrir o sistema da empresa
+# passo 2 : Fazer o login
+# passo 3 : importar a base de dados dos produtos
+# passo 4 : Cadastrar cada um dos produtos
+# passo 5 : Fechar o sistemada empresa
+
+#automatizar tarefas usando a biblioteca pyautogui
+#import pyautogui
+#pyautogui.click - comando para clicar
+#pyautogui.write - comando para escrever
+#pyautogui.press - comando para pressionar uma tecla
+#pyautogui.hotkey - comando para pressionar varias teclas ao mesmo tempo
+#replit para programar no celular
